a17ee801900720a5e6bc9ed72c088b878db43673
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 13:11:29 -0400

    Add border to the LoginRegister component

--- client/src/components/routes/application/LoginRegister.tsx
+++ client/src/components/routes/application/LoginRegister.tsx
@@ -4,7 +4,7 @@ import StyledButton from "../../ui/StyledButton"
 
 const LoginRegister = () => {
   return (
-    <Card twClasses="w-fit mx-auto p-20">
+    <Card twClasses="w-fit mx-auto p-20 border-4 border-secondary ">
       <Overview twClasses="space-y-10">
         <h1 className="text-5xl font-bold">Do you have an account with us?</h1>
         <div className="flex flex-row gap-10 justify-center">
