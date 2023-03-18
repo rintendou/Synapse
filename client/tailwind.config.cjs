e7e3087e5133278a48c5609dcc85a1eb38a76b8f
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 11:47:45 -0400

    Change the default font family

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
     extend: {
       colors: {
         primary: "#FFFFFF",
