67d6b38d63d3927482c3696ebff443908fbcab7d
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 13:34:22 -0400

    Implement the LoginForm component

--- client/src/components/routes/login/LoginForm.tsx
+++ client/src/components/routes/login/LoginForm.tsx
@@ -1,25 +1,26 @@
 import Card from "../../ui/Card"
-import Overview from "../../ui/Overview"
-import RouterLink from "../../ui/RouterLink"
+import StyledButton from "../../ui/StyledButton"
+import StyledInput from "../../ui/StyledInput"
 
 const LoginForm = () => {
+  const loginUserHandler = () => {}
+
   return (
-    <Card twClasses="w-fit mx-auto p-20 border-4 border-secondary ">
-      <Overview twClasses="space-y-10">
-        <h1 className="text-5xl font-bold">Do you have an account with us?</h1>
-        <div className="flex flex-row gap-10 justify-center">
-          <RouterLink
-            to="/login"
-            routerLinkText="Login"
-            twClasses="w-32 font-bold text-lg text-primary bg-secondary rounded-md py-2"
-          />
-          <RouterLink
-            to="/register"
-            routerLinkText="Register"
-            twClasses="w-32 font-bold text-lg text-secondary bg-primary rounded-md py-2 border-2 border-secondary"
-          />
-        </div>
-      </Overview>
+    <Card twClasses="w-fit mx-auto p-20 border-4 border-secondary space-y-20">
+      <h1 className="text-2xl font-bold text-center">Login</h1>
+      <div className="flex flex-col">
+        <StyledInput
+          name="Email Address"
+          type="email"
+          placeholder="Email Address"
+        />
+        <StyledInput name="Password" type="password" placeholder="Password" />
+      </div>
+      <StyledButton
+        buttonText="Login"
+        twClasses="w-full py-3"
+        onClick={loginUserHandler}
+      />
     </Card>
   )
 }
