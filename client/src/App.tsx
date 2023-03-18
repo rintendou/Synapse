d292859bb14eaf039aa5e38c2debdc7e0b899da2
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 11:10:25 -0400

    Install react-router-dom and implement the RouterLink component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,13 @@
+import RouterLink from "./components/ui/RouterLink"
+
 function App() {
-  return <div className="bg-primary">Hello WOrld!</div>
+  return (
+    <div className="bg-primary">
+      <RouterLink to="/" routerLinkText="Home"></RouterLink>
+      <RouterLink to="/test" routerLinkText="Home"></RouterLink>
+      <RouterLink to="/anotherroute" routerLinkText="Home"></RouterLink>
+    </div>
+  )
 }
 
 export default App
