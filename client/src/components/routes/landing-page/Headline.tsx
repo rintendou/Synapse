5cfbcbb4429216c3a258a409135c8dcbecf13d11
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 12:43:01 -0400

    Add padding on the y-axis to the Headline component

--- client/src/components/routes/landing-page/Headline.tsx
+++ client/src/components/routes/landing-page/Headline.tsx
@@ -1,6 +1,6 @@
 const Headline = () => {
   return (
-    <div className="flex flex-col text-center md:text-left md:flex-row items-center justify-center gap-16 bg-secondary text-primary cursor-pointer group">
+    <div className="flex flex-col-reverse text-center md:text-left md:flex-row items-center justify-center gap-16 bg-secondary text-primary cursor-pointer group p-10">
       <h1 className="text-4xl md:text-6xl font-bold tracking-wide max-w-[45%]">
         a minimalist and performant chat application.
       </h1>
