d4dc469e55172d7667022734bf9bc103bc3badba
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 14:12:28 -0400

    Implement functionality of the RegisterForm component:

--- client/src/components/routes/register/RegisterForm.tsx
+++ client/src/components/routes/register/RegisterForm.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 
 import Card from "../../ui/Card"
 import StyledButton from "../../ui/StyledButton"
@@ -11,7 +11,27 @@ const RegisterForm = () => {
   const emailAddressRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
 
-  const registerUserHandler = () => {}
+  // focus on the first input on component mount
+  useEffect(() => {
+    fullNameRef.current!.focus()
+  }, [])
+
+  const registerUserHandler = () => {
+    const registerUser = async () => {
+      const fullName = fullNameRef.current!.value
+      const emailAddress = emailAddressRef.current!.value
+      const password = passwordRef.current!.value
+
+      const response = await fetch("http://localhost:5000/register", {
+        method: "POST",
+        body: JSON.stringify({ fullName, emailAddress, password }),
+        headers: { "Content-Type": "application/json" },
+      })
+      const data = await response.json()
+      console.log(data)
+    }
+    registerUser()
+  }
 
   return (
     <Card twClasses="w-fit mx-auto p-20 border-4 border-secondary space-y-20">
