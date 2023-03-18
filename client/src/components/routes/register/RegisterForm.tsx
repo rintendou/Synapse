8889adae2487c6875c3e52b0a2373d506242b1da
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 14:04:22 -0400

    Implement the StyledInputRef component
    This is an important ui component where it accepts a ref for optimized input html elements

--- client/src/components/routes/register/RegisterForm.tsx
+++ client/src/components/routes/register/RegisterForm.tsx
@@ -1,21 +1,40 @@
+import { useRef } from "react"
+
 import Card from "../../ui/Card"
-import StyledInput from "../../ui/StyledInput"
 import StyledButton from "../../ui/StyledButton"
+import StyledInputRef from "../../ui/StyledInputRef"
 
 const RegisterForm = () => {
+  // I opted to use the useRef hook instead of useState to prevent
+  // unnecessary re-renders of this component per each character typed
+  const fullNameRef = useRef<HTMLInputElement>(null)
+  const emailAddressRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
   const registerUserHandler = () => {}
 
   return (
     <Card twClasses="w-fit mx-auto p-20 border-4 border-secondary space-y-20">
       <h1 className="text-4xl font-bold text-center">Register</h1>
       <div className="flex flex-col">
-        <StyledInput name="Full Name" type="text" placeholder="Full Name" />
-        <StyledInput
+        <StyledInputRef
+          name="Full Name"
+          type="text"
+          placeholder="Full Name"
+          ref={fullNameRef}
+        />
+        <StyledInputRef
           name="Email Address"
           type="email"
           placeholder="Email Address"
+          ref={emailAddressRef}
         />
-        <StyledInput name="Password" type="password" placeholder="Password" />
+        <StyledInputRef
+          name="Password"
+          type="password"
+          placeholder="Password"
+          ref={passwordRef}
+        />
       </div>
       <StyledButton
         buttonText="Register"
