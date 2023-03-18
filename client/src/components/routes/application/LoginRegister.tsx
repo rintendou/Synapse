05f647488c24c8cf4115ff92140a777033e390c4
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-18 13:15:11 -0400

    Replace StyledButton components to RouterLink components
    The latter component fits the expected functionality of the LoginRegister route/component. The user will be routed to whichever link they click: login route or register route

--- client/src/components/routes/application/LoginRegister.tsx
+++ client/src/components/routes/application/LoginRegister.tsx
@@ -1,5 +1,6 @@
 import Card from "../../ui/Card"
 import Overview from "../../ui/Overview"
+import RouterLink from "../../ui/RouterLink"
 import StyledButton from "../../ui/StyledButton"
 
 const LoginRegister = () => {
@@ -8,21 +9,15 @@ const LoginRegister = () => {
       <Overview twClasses="space-y-10">
         <h1 className="text-5xl font-bold">Do you have an account with us?</h1>
         <div className="flex flex-row gap-10 justify-center">
-          <StyledButton
-            buttonText="Login"
-            intent="primary"
-            onClick={() => {
-              console.log("Hello")
-            }}
-            twClasses="w-32 font-bold text-lg"
+          <RouterLink
+            to="/login"
+            routerLinkText="Login"
+            twClasses="w-32 font-bold text-lg text-primary bg-secondary rounded-md py-2"
           />
-          <StyledButton
-            buttonText="Register"
-            intent="secondary"
-            onClick={() => {
-              console.log("Hello")
-            }}
-            twClasses="w-32 font-bold text-lg"
+          <RouterLink
+            to="/register"
+            routerLinkText="Register"
+            twClasses="w-32 font-bold text-lg text-secondary bg-primary rounded-md py-2 border-2 border-secondary"
           />
         </div>
       </Overview>
