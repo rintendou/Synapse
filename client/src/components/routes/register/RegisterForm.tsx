1c1b9b6ffd3722866a07110aaf0244ecc1879dfd
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-19 12:42:58 -0400

    Fix RegisterForm component by sending a request to the backend_server_port

--- client/src/components/routes/register/RegisterForm.tsx
+++ client/src/components/routes/register/RegisterForm.tsx
@@ -4,8 +4,6 @@ import Card from "../../ui/Card"
 import StyledButton from "../../ui/StyledButton"
 import StyledInputRef from "../../ui/StyledInputRef"
 
-require("dotenv").config({ path: '../.env'}) // BROKEN IDK WHY
-
 const RegisterForm = () => {
   // I opted to use the useRef hook instead of useState to prevent
   // unnecessary re-renders of this component per each character typed
@@ -24,7 +22,7 @@ const RegisterForm = () => {
       const emailAddress = emailAddressRef.current!.value
       const password = passwordRef.current!.value
 
-      const response = await fetch(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/api/auth/register`, {
+      const response = await fetch(`http://localhost:5000/api/auth/register`, {
         method: "POST",
         body: JSON.stringify({
           username: fullName,
