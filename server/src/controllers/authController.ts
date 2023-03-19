ca9473da579abfd5a8992f4e63dd914c13316ad8
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-19 13:07:20 -0400

    Reformat response structure to follow a specific format
    response object = { message: 'someMessage', data: someData, ok: true}

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -21,9 +21,13 @@ export const registerUser = async (req: Request, res: Response) => {
 
     // Saving new User
     const registeredUser = await user.save()
-    res.status(200).json(registeredUser)
+    res.status(200).json({
+      message: "User successfully registered!",
+      data: registeredUser,
+      ok: true,
+    })
   } catch (error) {
-    res.status(500).json(error)
+    res.status(500).json({ message: error, data: null, ok: false })
   }
 }
 
@@ -38,17 +42,21 @@ export const loginUser = async (req: Request, res: Response) => {
       email: req.body.email,
     })
 
-    if (!user) res.status(400).json("User not found")
+    if (!user)
+      res.status(400).json({ message: "User not found", data: null, ok: false })
 
     const validPassword = await bcrypt.compare(
       req.body.password,
       user!.password
     )
 
-    if (!validPassword) res.status(400).json("Wrong password")
+    if (!validPassword)
+      res.status(400).json({ message: "Wrong password", data: null, ok: false })
 
-    return res.status(200).json(user)
+    return res
+      .status(200)
+      .json({ message: "Login Success!", data: user, ok: true })
   } catch (error) {
-    return res.status(500).json(error)
+    return res.status(500).json({ message: error, data: null, ok: false })
   }
 }
--- server/src/controllers/chatController.ts
+++ server/src/controllers/chatController.ts
@@ -1,36 +1,44 @@
-import { Request, Response } from "express";
-import ChatModel from "../models/Chat";
+import { Request, Response } from "express"
+import ChatModel from "../models/Chat"
 
 export const createChat = async (req: Request, res: Response) => {
-	/* 
+  /* 
 	Create a new chat using the ChatModel.
 	Inside the request body, there are two keys: "senderId" & "receiverId". The ID's should be the ObjectId of the users in the DB.
 	*/
-	const newChat = new ChatModel({
-		members: [req.body.senderId, req.body.receiverId],
-	});
+  const newChat = new ChatModel({
+    members: [req.body.senderId, req.body.receiverId],
+  })
 
-	try {
-		const savedChat = await newChat.save();
-		return res.status(200).json(savedChat);
-	} catch (error) {
-		return res.status(500).json(error);
-	}
-};
+  try {
+    const savedChat = await newChat.save()
+    return res.status(200).json({
+      message: "Chat successfully created!",
+      data: savedChat,
+      ok: true,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error, data: null, ok: false })
+  }
+}
 
 export const getUserChat = async (req: Request, res: Response) => {
-	/* 
+  /* 
 	Retreive a chat using the ChatModel.
 	Inside the params (the URL), the user's unqiue ObjectId will be the parameter.
 	$in is a query: https://www.mongodb.com/docs/manual/reference/operator/query/in/
 	*/
 
-	try {
-		const chat = await ChatModel.find({
-			members: { $in: [req.params.userId] },
-		});
-		return res.status(200).json(chat);
-	} catch (error) {
-		return res.status(500).json(error);
-	}
-};
+  try {
+    const chat = await ChatModel.find({
+      members: { $in: [req.params.userId] },
+    })
+    return res.status(200).json({
+      message: "User chat successfully fetched!",
+      data: chat,
+      ok: true,
+    })
+  } catch (error) {
+    return res.status(500).json({ message: error, data: null, ok: false })
+  }
+}
