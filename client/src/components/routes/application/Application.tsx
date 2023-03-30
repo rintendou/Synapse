e0c3eeafed59f891e928b3f35d736b850230cda6
Kennette James Basco <kenji.maddela01@gmail.com>
2023-03-29 23:09:07 -0400

    Test useAuth hook

--- client/src/components/routes/application/Application.tsx
+++ client/src/components/routes/application/Application.tsx
@@ -1,6 +1,11 @@
+import useAuth from "../../../lib/hooks/useAuth"
 import LoginRegister from "./LoginRegister"
 
 const Application = () => {
+  const { user } = useAuth()
+
+  console.log(user)
+
   return (
     <div className="min-h-screen flex flex-col justify-center">
       <LoginRegister />
